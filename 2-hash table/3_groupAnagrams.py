c0feac7b8492c3a14ba80e2f20aea7211dd3c532
JZhaolei <32220390+JZhaolei@users.noreply.github.com>
2018-03-13 15:51:48 +0800

    3-13
    3-13

--- 2-hash table/3_groupAnagrams.py
+++ 2-hash table/3_groupAnagrams.py
@@ -8,3 +8,17 @@ class Solution(object):
         for s in strs:
             ans[tuple(sorted(s))].append(s)
         return ans.values()
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
